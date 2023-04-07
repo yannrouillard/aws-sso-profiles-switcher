d9d260f03af0b28fb61960167cf0adc9a0f0e905
Yann Rouillard <yann@pleiades.fr.eu.org>
2023-04-08 00:27:51 +0200

    :sparkles: allow to auto-populate with used AWS profiles

---
+++ src/background_scripts/background.js
@@ -0,0 +1,71 @@
+const REQUIRED_PERMISSIONS = {
+  origins: ["https://*.amazonaws.com/federation/console*"],
+};
+
+function extractProfileInfoFromRequest(requestDetails) {
+  const url = new URL(requestDetails.url);
+  const profileName = url.searchParams.get("role_name");
+  const accountId = url.searchParams.get("account_id");
+  const originUrl = new URL(requestDetails.originUrl);
+  const portalDomain = originUrl.hostname.split(".")[0];
+  // The hash part loolk like this /saml/custom/<ACCOUND_ID> (<ACCOUNT_NAME>)/<BASE64_STRING>
+  // so we can extract the account name from it
+  const [, accountName] = decodeURIComponent(originUrl.hash.split("/")[3]).match(/\((.*)\)$/);
+
+  const profileInfo = {
+    portalDomain,
+    accountName,
+    accountId,
+    name: profileName,
+    url: requestDetails.originUrl,
+    title: `${accountName} - ${profileName}`,
+    id: `${portalDomain} - ${accountName} - ${profileName}`,
+  };
+  return profileInfo;
+}
+
+async function saveAwsProfile(awsProfile) {
+  const storageContent = await browser.storage.local.get({ awsProfiles: {} });
+  const existingAwsProfile = storageContent.awsProfiles[awsProfile.id] || {};
+  storageContent.awsProfiles[awsProfile.id] = Object.assign({}, existingAwsProfile, awsProfile);
+  await browser.storage.local.set(storageContent);
+}
+
+function signInfoRequestListener(requestDetails) {
+  const awsProfile = extractProfileInfoFromRequest(requestDetails);
+  saveAwsProfile(awsProfile);
+  return {};
+}
+
+async function configureListener() {
+  const configuration = (await browser.storage.local.get({ configuration: {} })).configuration;
+  const hasPermissions = await browser.permissions.contains(REQUIRED_PERMISSIONS);
+  const alreadyRegistered = browser.webRequest.onBeforeRequest.hasListener(signInfoRequestListener);
+
+  if (configuration.autoPopulateUsedProfiles && hasPermissions) {
+    if (!alreadyRegistered) {
+      browser.webRequest.onBeforeRequest.addListener(signInfoRequestListener, {
+        urls: REQUIRED_PERMISSIONS["origins"],
+        types: ["xmlhttprequest"],
+      });
+    }
+  } else {
+    if (alreadyRegistered) {
+      browser.webRequest.onBeforeRequest.removeListener(signInfoRequestListener);
+    }
+  }
+}
+
+async function handleMessage(request) {
+  if (request.command == "reloadBackgroundScript") {
+    await configureListener();
+  }
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
+
+// We run immediately the configureListener to be sure this non-persistent page
+// is properly registered to be woken up in case of watched webrequests
+(async () => {
+  await configureListener();
+})();
