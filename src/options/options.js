b0a80b16a8992296bdff218d83db982b7993fabc
Yann Rouillard <yann@pleiades.fr.eu.org>
2023-04-08 00:29:17 +0200

    :fire: remove default container option not really useful
    This option will be overriden if the user selects a default
    profile for the SSO portal page in Firefox Multi-Containers
    extension, so it's better than the user simply uses directly
    Firefox Multi-Containers if he wants such thing.

--- src/options/options.js
+++ src/options/options.js
@@ -3,27 +3,6 @@ const PERMISSIONS = {
 };
 
 async function setupOptionsUI() {
-  const defaultContainer = (await browser.storage.local.get("defaultContainer")).defaultContainer;
-  const defaultContainerOption = document.querySelector("select#default_container");
-  const availableIdentities = await browser.contextualIdentities.query({});
-  const availableContainerOptions = availableIdentities.map(
-    (identity) =>
-      new Option(
-        identity.name,
-        identity.cookieStoreId,
-        false,
-        identity.cookieStoreId == defaultContainer
-      )
-  );
-  for (const option of availableContainerOptions) {
-    defaultContainerOption.add(option);
-  }
-
-  defaultContainerOption.addEventListener("change", async (event) => {
-    const cookieStoreId = event.target.value == "default" ? undefined : event.target.value;
-    await browser.storage.local.set({ defaultContainer: cookieStoreId });
-  });
-
   const hasPermissions = await browser.permissions.contains(PERMISSIONS);
   const storageContent = await browser.storage.local.get({ configuration: {} });
   const configuration = storageContent.configuration;
